e3b6c425bb996168ae840d11f80173260cf45b31
Volodymyr Shved <cruigma@gmail.com>
2020-11-25 11:44:15 +0200

    Add lesson 25 task

---
+++ Lesson25.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * Refactor this
+ * https://imgur.com/xwtTMrY
+*/
+namespace CBasics
+{
+    public class Refactoring
+    {
+        public static void Solution()
+        {
+            double value;
+            var g = 0;
+            const int x = 1, y = 2, z = 3;
+
+            Console.WriteLine("Введите число");
+            var input = Convert.ToUInt32(Console.ReadLine());
+
+            if (x > y && input < 15)
+            {
+                g = y;
+                value = g + Math.PI;
+            }
+            else
+            {
+                if (z == y && z < 4) g = z;
+                value = g + Math.PI * 2;
+            }
+
+            Console.WriteLine($"Число g = {g}, значение value = {value}");
+        }
+    }
+}

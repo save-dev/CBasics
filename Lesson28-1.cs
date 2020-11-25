7f6b01d1fd7f452b120056bf6046d08085a27e8e
Volodymyr Shved <cruigma@gmail.com>
2020-11-25 19:41:45 +0200

    Add Task 28-1

---
+++ Lesson28-1.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * https://imgur.com/yVNNIzi
+ */
+
+using System;
+
+namespace CBasics
+{
+    public class Lesson281
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Введите два целых числа (через клавишу Enter)");
+            var x = Convert.ToInt32(Console.ReadLine());
+            var y = Convert.ToInt32(Console.ReadLine());
+
+            x = (x % 2 == 0) ? x : ++x;
+            y = (y % 2 == 0) ? y : ++y;
+
+            var result = ((x + y) % 4 == 0)
+                ? $"Сумма значений {x} и {y} делится на 4"
+                : $"Сумма значений {x} и {y} на 4 не делится";
+
+            Console.WriteLine(result);
+        }
+    }
+}

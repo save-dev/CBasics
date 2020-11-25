4a75d358c0837427ec1a74ca3132f71de54f1a94
Volodymyr Shved <cruigma@gmail.com>
2020-11-25 20:21:36 +0200

    Add Task 28-2 (pdzc)

---
+++ Lesson28-2.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * https://imgur.com/yVNNIzi
+ */
+
+using System;
+
+namespace CBasics
+{
+    public class Lesson282
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Введите два целых числа (через клавишу Enter)");
+            var x = Convert.ToInt32(Console.ReadLine());
+            var y = Convert.ToInt32(Console.ReadLine());
+
+            (x >= y ? ref x : ref y) = 100;
+
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+    }
+}

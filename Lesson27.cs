53a28f37ee41c904f87f8856ef79a0e349b1355b
Volodymyr Shved <cruigma@gmail.com>
2020-11-25 19:08:04 +0200

    Add Task 27

---
+++ Lesson27.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CBasics
+{
+    /*
+     * https://imgur.com/5PWFdZX
+     */
+    public class Lesson27
+    {
+        public static void Solution()
+        {
+            double x, y, res;
+            char op;
+            {
+                Console.WriteLine("Введите два операнда (разделяя клавишей Enter)");
+                x = Convert.ToDouble(Console.ReadLine());
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите оператор");
+                op = Convert.ToChar(Console.ReadLine());
+            }
+            checked
+            {
+                switch (op)
+                {
+                    case '+':
+                        res = x + y;
+                        break;
+                    case '-':
+                        res = x - y;
+                        break;
+                    case '*':
+                        res = x * y;
+                        break;
+                    case '/':
+                        if (y == 0)
+                        {
+                            Console.WriteLine("Деление на ноль.");
+                            return;
+                        }
+                        else res = x / y;
+
+                        break;
+                    default:
+                        Console.WriteLine("Нет такой операции");
+                        return;
+                }
+            }
+
+
+            Console.WriteLine(res);
+        }
+    }
+}

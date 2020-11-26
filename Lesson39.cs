a20ef4e84eb0a31c760ba48a9e75b3f7431042d2
Volodymyr Shved <cruigma@gmail.com>
2020-11-26 03:02:34 +0200

    Add tasks 39

---
+++ Lesson39.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * https://imgur.com/tvttsy4
+ */
+
+using System;
+
+namespace CBasics
+{
+    public class Lesson39
+    {
+        public static void Solution()
+        {
+        }
+
+        private static void PrintResult(int m, int n, int k)
+        {
+            Console.WriteLine($"{m + n + k}");
+        }
+
+        private static void PrintResult(ref int m, ref int n, ref int k)
+        {
+            Console.WriteLine($"{m - n - k}");
+        }
+
+        private static void PrintResult(int m, int n)
+        {
+            Console.WriteLine(Math.Pow(m, n));
+        }
+
+    // TODO: One more override
+    }
+}

fc484e5b9a60d6eb306d3b7fec5e659c81518618
Volodymyr Shved <cruigma@gmail.com>
2020-11-26 02:34:58 +0200

    Add tasks 37-38

---
+++ Lesson37.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * https://imgur.com/l9KkiIi
+ */
+
+using System;
+
+namespace CBasics
+{
+    public class Lesson37
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Введите количество сотрудников предприятия");
+            var numberEmployees = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость одного трудодня");
+            var workDayRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество дней для оплаты");
+            var totalDays = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Общий объем денег, необходимый для оплаты всем сотрудникам: {GrossSalary(numberEmployees, totalDays, workDayRate)}");
+        }
+
+        /// <summary>
+        /// Зарплата одного сотрудника
+        /// </summary>
+        /// <param name="totalDays">Рабочих дней</param>
+        /// <param name="workDayRate">Оплата за день</param>
+        /// <returns>ЗП сотрудника</returns>
+        private static int EmployeeSalary(int totalDays, int workDayRate)
+        {
+            return checked(totalDays * workDayRate);
+        }
+
+        /// <summary>
+        /// Зарплата всех сотрудников
+        /// </summary>
+        /// <param name="numberEmployees">Кол-во сотрудников</param>
+        /// <param name="totalDays">Рабочих дней</param>
+        /// <param name="workDayRate">Оплата за день</param>
+        /// <returns>Зарплата всех сотрудников</returns>
+        private static int GrossSalary(int numberEmployees, int totalDays, int workDayRate)
+        {
+            return checked(EmployeeSalary(totalDays, workDayRate) * numberEmployees);
+        }
+    }
+}
---
+++ Lesson38.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * https://imgur.com/b2TKGDV
+ */
+
+using System;
+
+namespace CBasics
+{
+    public class Lesson38
+    {
+        public static void Solution()
+        {
+            int x = 1, y = 2, z = 3;
+            var b = false;
+            Console.WriteLine($"Результат выполнения метода: {Chaos(x, y, z, b)}");
+            Console.WriteLine($"Значения параметров x, y, z, bool: {x}, {y}, {z}, {b}");
+        }
+
+        private static int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        private static int BiggerSum(ref int x, ref int y, ref int z)
+        {
+            x++;
+            y++;
+            z++;
+            return x + y + z;
+            // TODO: WTF with those guys x, y and z
+        }
+
+        private static int Chaos(int x, int y, int z, bool b)
+        {
+            return b ? Sum(x, y, z) : BiggerSum(ref x, ref y, ref z);
+        }
+    }
+}
